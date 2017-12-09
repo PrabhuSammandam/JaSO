533f5f72976bdd87de999915d0a0c1b97cb7a0c3
Prabhu Sammandam <prabhusammandam@gmail.com>
2017-12-09 10:00:18 +0530

    Added SimpleServer

---
+++ apps/SimpleServer/ServerExample.cpp
@@ -0,0 +1,25 @@
+#include "ServerDataChannel.h"
+#include "CoapServer.h"
+#include "JaCoapIF.h"
+
+using namespace JaCoAP;
+
+FILE_NAME( "ServerExample.cpp" )
+
+int main()
+{
+  dbg_info( "Starting" );
+  auto         serverDataChannel = mnew ServerDataChannel();
+
+  CoapServer   coapServer( serverDataChannel );
+
+  IpSocketAddr ipSockAddr( u32( 0 ), 5683 );
+
+  coapServer.AddEp( ipSockAddr );
+
+  coapServer.Start();
+
+  coapServer.Loop();
+
+  return ( 0 );
+}
